95267a61d5c203d3352d9555925828c58bd330b9
Rahul Kulkarni <63799217+rahul101001000@users.noreply.github.com>
2022-09-18 22:35:42 +0530

    Added input argument assignment to a and b

---
+++ addition-input.py
@@ -0,0 +1,9 @@
+# Complete this program to add two numbers with inputs
+
+from sys import argv
+
+a = int(argv[1])
+b = int(argv[2])
+c = # ...?
+print(c)
+
--- addition.py
+++
@@ -1,7 +0,0 @@
-# Complete this program to add two numbers. 
-
-a = 1
-b = 2
-c = # ...?
-print(c)
-
